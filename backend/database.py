e31f9c8adf8ff61dfc71b6235c5cf55e851248ee
whyvickyyy <tyagivivek528@gmail.com>
2026-06-17 09:26:55 +0530

    chores:added docker compose

--- backend/database.py
+++ backend/database.py
@@ -5,13 +5,13 @@ from sqlalchemy.orm import DeclarativeBase
 from dotenv import load_dotenv
 
 load_dotenv()
-DB_URL = os.getenv("DB_URL")
+ASYNC_DB_URL = os.getenv("ASYNC_DB_URL")
 
-if not DB_URL:
-    raise ValueError("DB_URL env var is not set")
+if not ASYNC_DB_URL:
+    raise ValueError("ASYNC_DB_URL env var is not set")
 
 # creates async engine for connecting to the database using the URL from the environment variable and async because we are using asyncpg as the driver. The echo=True option enables logging of SQL statements, and pool_pre_ping=True ensures that connections are checked for liveness before being used from the pool.
-async_engine = create_async_engine(DB_URL, echo=True, pool_pre_ping=True)
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True, pool_pre_ping=True)
 
 async_session_maker = async_sessionmaker(
     bind=async_engine,
